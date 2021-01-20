479008df3522e23c7e7d5e156637fe348091a3a8
Dung Ho <mclanren156@gmail.com>
2021-01-20 07:00:44 +0700

    168. Restructuring Actions

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionCreator from '../../store/actions/actions';
+import * as actionCreator from '../../store/actions/index';
 
 class Counter extends Component {
     state = {
